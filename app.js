3d2b0494552d7fcc5887f85b337e19cdb18363c2
Kentaro Takashima <himuhimuphone@gmail.com>
2016-12-18 23:47:27 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var basicAuth = require('basic-auth-connect');
+var settingconfig = require('./config.json');
+
+var index = require('./routes/index');
+var workshop = require('./routes/workshop');
+var data_upload = require('./routes/data_upload');
+var report = require('./routes/report');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(basicAuth(settingconfig.basicAuth.user, settingconfig.basicAuth.pass));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/workshop', workshop);
+app.use('/data_upload', data_upload);
+app.use('/report', report);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
